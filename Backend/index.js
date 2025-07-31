f5979d306b550a37741708783d42783e7c700dd5
Shiv Jagath <itsshivjagath@gmail.com>
2025-07-31 06:16:12 +0200

    update for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -73,10 +73,10 @@ app.get('/api/search', authenticateToken, async (req, res) => {
 
 // static files from build
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'Frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../Frontend/dist')));
   // app.get('/*', (req, res) => {
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'Frontend/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
   });
 }
 
